f5d489b1c939eb288ffa3c5f2fb0c7064a3a0e2e
eputrap <eputrap@gmail.com>
2024-07-28 14:30:56 +0700

    remove auth

--- routes/route.go
+++ routes/route.go
@@ -29,7 +29,7 @@ func Build(srv *gin.Engine, h Handlers, middlewares Middlewares) {
 	userCurrentLocation := srv.Group("/user-location")
 	userCurrentLocation.PUT("/update-sos", middlewares.Auth, h.UserCurrentLocation.UpdateSOS)
 	userCurrentLocation.PUT("/update", middlewares.Auth, h.UserCurrentLocation.UpdateUserCurrentLocation)
-	userCurrentLocation.GET("/users-location", middlewares.Auth, h.UserCurrentLocation.GetCurrentUserLocation)
+	userCurrentLocation.GET("/users-location", h.UserCurrentLocation.GetCurrentUserLocation)
 
 	test := srv.Group("test")
 	test.GET("/", middlewares.Auth, h.Test.Test)
